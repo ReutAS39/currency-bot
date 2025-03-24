3af5d9291a5b56338db9b2b61f3a59e94ae1682f
ReutAS <reutas39@gmail.com>
2025-03-24 13:50:02 +0200

    tune

--- bot/config.py
+++ bot/config.py
@@ -1,15 +1,15 @@
 import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
 class Settings(BaseSettings):
     BOT_TOKEN: str
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
     )
 
-# Получаем параметры для загрузки переменных среды
-settings = Settings()
 
+settings = Settings()
 
 
 currencys = {
